96bbf36015ecefb8d9e7f436474b01540c715754
Nicolás Nieto <niconieto1506@gmail.com>
2021-04-12 16:38:46 -0500

    Paquete

--- src/main/java/co/edu/unicundi/carrera/Main.java
+++ src/main/java/co/edu/unicundi/carrera/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.carrerarelevos;
+package co.edu.unicundi.carrera;
 
 /**
  *
@@ -12,7 +12,7 @@ package com.mycompany.carrerarelevos;
 public class Main {
     
     public static void main(String[] args) {
-        
+        System.out.println("Hola");
     }
     
 }
