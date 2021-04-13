497cec3337fd353e721f813ef5e07140fa2ca0e4
ltramos14 <tatianarv14@gmail.com>
2021-04-12 22:45:04 -0500

    Primera parte de implementación de hilos en la clase Main

--- src/main/java/co/edu/unicundi/carrera/Main.java
+++ src/main/java/co/edu/unicundi/carrera/Main.java
@@ -5,19 +5,74 @@
  */
 package co.edu.unicundi.carrera;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ *Clase principal que inicia el programa de la carrera de relevos
  *
- * @author nicon
+ * @author Tatiana Ramos Villanueva
+ * @author Nicolás Nieto Cárdenas
+ * @version 1.1.0
+ * @since 1.0.0
  */
 public class Main {
+    
+    Racer racer1;
+    Racer racer2;
+    Racer racer3;
+    Thread threadRacer1;
+    Thread threadRacer2;
+    Thread threadRacer3;
 
     public Main() {
-        
+                  
+        racer1 = new Racer(1);
+        threadRacer1 = new RedTeam(racer1);
+
+        racer2 = new Racer(2);
+        threadRacer2 = new BlueTeam(racer2);
+
+        racer3 = new Racer(3);
+        threadRacer3 = new YellowTeam(racer3);
+
+        threadRacer1.start();
+        threadRacer2.start();
+        threadRacer3.start();
+
+        while (threadRacer1.isAlive() && threadRacer2.isAlive() && threadRacer3.isAlive()) {
+
+            if (racer1.getPosition() >= 20) {
+                threadRacer1.interrupt();
+                threadRacer2.interrupt();
+                threadRacer3.interrupt();
+
+                System.out.println("EL EQUIPO GANADOR ES EL EQUIPO ROJO");
+                break;
+
+            } else if (racer2.getPosition() >= 20) {
+
+                threadRacer1.interrupt();
+                threadRacer2.interrupt();
+                threadRacer3.interrupt();
+
+                System.out.println("EL EQUIPO GANADOR ES EL EQUIPO AZUL");
+                break;
+
+            } else if (racer3.getPosition() >= 20) {
+
+                threadRacer1.interrupt();
+                threadRacer2.interrupt();
+                threadRacer3.interrupt();
+
+                System.out.println("EL EQUIPO GANADOR ES EL EQUIPO AMARILLO");
+                break;
+            }
+        }
     }
- 
+
     public static void main(String[] args) {
-        new Main();	        
-        System.out.println("\033[35mTexto en Verde");
+        new Main();
     }
-    
+
 }
--- src/main/java/co/edu/unicundi/carrera/Racer.java
+++ src/main/java/co/edu/unicundi/carrera/Racer.java
@@ -9,41 +9,51 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * Clase Hija de Figura que calcula área y perímetro del círculo
  *
- * @author nicon
+ * @author Tatiana Ramos Villanueva
+ * @author Nicolás Nieto Cárdenas
+ * @version 1.1.0
+ * @since 1.0.0
  */
 public class Racer {
 
     private int racerNumber;
+    private int position;
 
     public Racer(int racerNumber) {
         this.racerNumber = racerNumber;
+        this.position = 0;
     }
 
     public void moveRacer(String teamName, String color) {
         try {
-            
-            int position = 0;
+
             int radomNumber;
 
-            while (position < 20) {
+            while (position <= 20) {
 
-                radomNumber = generateRandomNumber(1, 3);
-                System.out.println(color + teamName + "Corredor " + racerNumber + "se ha movido " + radomNumber + " posiciones.");
+                radomNumber = generateRandomNumber(1, 4);
                 position += radomNumber;
 
-                Thread.sleep(1500);
-                
+                System.out.println(color + teamName + "Corredor " + racerNumber + " esta en la posición " + position);
+
+                Thread.sleep(1000);
+
             }
         } catch (InterruptedException ex) {
             Logger.getLogger(Racer.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
 
     public static int generateRandomNumber(int numberMin, int numberMax) {
         int randomNumber = (int) Math.floor(Math.random() * (numberMax - numberMin + 1) + (numberMin));
         return randomNumber;
+    }
+
+    public int getPosition() {
+        return position;
     }
 
 }
